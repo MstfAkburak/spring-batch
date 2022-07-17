9cdfcc9b82e70aea1c906946be71a8dde80031a0
Mustafa Akburak <mustafaakburak@192.168.1.70>
2022-07-18 01:50:35 +0300

    Initial commit

---
+++ src/main/java/com/example/springbatch/entity/Customer.java
@@ -0,0 +1,34 @@
+package com.example.springbatch.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @Column(name = "CUSTOMER_ID")
+    private int id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "EMAİL")
+    private String email;
+    @Column(name = "GENDER")
+    private String gender;
+    @Column(name = "CONTACT")
+    private String contactNo;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "DOB")
+    private String dob;
+}
